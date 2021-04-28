1c1ff993ba79b4d72099b306df48dacec7e3a5b4
capem76 <capem76@hotmail.com>
2021-04-28 19:26:04 +0200

    primer commit, configuracion spring boot y mysql finalizada

---
+++ spring-boot-backend-apirest/src/test/java/com/capename/springboot/cackend/apirest/SpringBootBackendApirestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capename.springboot.cackend.apirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBackendApirestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
