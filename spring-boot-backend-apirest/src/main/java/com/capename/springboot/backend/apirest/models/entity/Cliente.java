2cc6cf7c52f72a146b35ffbf98599b35065e9cfa
capem76 <capem76@hotmail.com>
2021-04-29 17:40:51 +0200

    se crea el cliente controller y la entity y el fichero sql para para
    datos de prueba.

--- spring-boot-backend-apirest/src/main/java/com/capename/springboot/backend/apirest/models/entity/Cliente.java
+++ spring-boot-backend-apirest/src/main/java/com/capename/springboot/backend/apirest/models/entity/Cliente.java
@@ -20,9 +20,11 @@ public class Cliente implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
 	private String nombre;
 	private String apellido;
 	private String email;
