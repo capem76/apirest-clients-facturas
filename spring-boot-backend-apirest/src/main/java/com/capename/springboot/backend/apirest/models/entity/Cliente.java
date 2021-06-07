c12fdbb842ee0deadfbbacf475d8ac713f875e2f
capem76 <capem76@hotmail.com>
2021-06-07 17:37:42 +0200

    se crean las entities para las relaciones entre Factura, detalle de
    facturas (ItemFactura) y productos. Se configura las clases para evitar
    el loop infinito cuand se crea el json y los atributos basura en las
    relaciones.

--- spring-boot-backend-apirest/src/main/java/com/capename/springboot/backend/apirest/models/entity/Cliente.java
+++ spring-boot-backend-apirest/src/main/java/com/capename/springboot/backend/apirest/models/entity/Cliente.java
@@ -1,8 +1,11 @@
 package com.capename.springboot.backend.apirest.models.entity;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -11,6 +14,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -19,6 +23,8 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import net.bytebuddy.asm.Advice.This;
 
 @Entity
 @Table(name = "clientes")
@@ -59,16 +65,26 @@ public class Cliente implements Serializable {
 	@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 	private Region region;
 	
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "cliente", cascade = CascadeType.ALL)
+	@JsonIgnoreProperties({"cliente", "hibernateLazyInitializer", "handler"})
+	private List<Factura> facturas;
+	
 		
 //	@PreUpdate
 //	public void preUpdate() {
 //		createAt = new Date();
 //	}
 	
+	
+	public Cliente() {
+		this.facturas = new ArrayList<>();
+	}
+	
 
 	public Long getId() {
 		return id;
 	}
+
 
 	public void setId(Long id) {
 		this.id = id;
@@ -123,13 +139,22 @@ public class Cliente implements Serializable {
 	public void setRegion(Region region) {
 		this.region = region;
 	}
+	
+	public List<Factura> getFacturas() {
+		return facturas;
+	}
 
+	public void setFacturas(List<Factura> facturas) {
+		this.facturas = facturas;
+	}
+	
 	@Override
 	public String toString() {
 		return "Cliente [id=" + id + ", nombre=" + nombre + ", apellido=" + apellido + ", email=" + email
-				+ ", createAt=" + createAt + ", foto=" + foto + ", region=" + region + "]";
+				+ ", createAt=" + createAt + ", foto=" + foto + ", region=" + region + ", facturas=" + facturas + "]";
 	}
 
+	
 	
 
 
--- spring-boot-backend-apirest/src/main/resources/import.sql
+++ spring-boot-backend-apirest/src/main/resources/import.sql
@@ -46,4 +46,26 @@
  INSERT INTO `usuario_roles` (usuario_id, role_id) VALUES (2, 2);
  INSERT INTO `usuario_roles` (usuario_id, role_id) VALUES (2, 1);
  
+ /* Populate tabla productos */
+ INSERT INTO productos (nombre, precio, create_at) VALUES ('Panasonic Pantalla LCD', 259990, now() );
+ INSERT INTO productos (nombre, precio, create_at) VALUES ('Sony Camara digital DSC-W320B', 123490, now() );
+ INSERT INTO productos (nombre, precio, create_at) VALUES ('Apple ipod shuffle', 1499990, now() );
+ INSERT INTO productos (nombre, precio, create_at) VALUES ('Sony Notebook Z119', 37990, now() );
+ INSERT INTO productos (nombre, precio, create_at) VALUES ('Hewlett Packard Multifuncional F2280', 69990, now() );
+ INSERT INTO productos (nombre, precio, create_at) VALUES ('Bianchi Bicicleta Aro 26', 69990, now() );
+ INSERT INTO productos (nombre, precio, create_at) VALUES ('Mica Comoda 5 cajones', 299990, now() );
+ INSERT INTO productos (nombre, precio, create_at) VALUES ('Samsung smartphone A23', 588521, now() );
+ 
+ /* Populate algunas facturas */
+ INSERT INTO  facturas ( descripcion, observacion, cliente_id, create_at ) VALUES ( 'Factura equipos de oficina', null, 1, now() ); 
+ INSERT INTO  facturas_items ( cantidad, factura_id, producto_id) VALUES (1, 1, 1);
+ INSERT INTO  facturas_items ( cantidad, factura_id, producto_id) VALUES (2, 1, 4);
+ INSERT INTO  facturas_items ( cantidad, factura_id, producto_id) VALUES (1, 1, 5);
+ INSERT INTO  facturas_items ( cantidad, factura_id, producto_id) VALUES (1, 1, 7);
+ 
+ INSERT INTO  facturas ( descripcion, observacion, cliente_id, create_at ) VALUES ( 'Factura de Bicicleta', 'Alguna nota importante!', 1, now() ); 
+ INSERT INTO facturas_items ( cantidad, factura_id, producto_id) VALUES (3, 2, 6);
+ 
+ 
+ 
  
